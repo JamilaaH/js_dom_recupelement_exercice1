32baa42158f9e86c569ba1d8e311d86c375222a8
Jamila <jaja_mimi@hotmail.com>
2021-01-26 09:57:25 +0100

    rectif'

--- main.js
+++ main.js
@@ -3,19 +3,17 @@ let titre = document.getElementsByTagName('h1');
 
 console.log(titre[0]);
 //2.
-let soustitre1 = document.getElementsByTagName('h3')[0];
-let soustitre2 = document.getElementsByTagName('h3')[1];
-let soustitre3 = document.getElementsByTagName('h3')[2];
+let soustitre = document.getElementsByTagName('h3');
 
-console.log(soustitre1);
-console.log(soustitre2);
-console.log(soustitre3);
+console.log(soustitre1[0]);
+console.log(soustitre2[1]);
+console.log(soustitre3[2]);
 
 //3.
-let paragraphe = document.getElementsByTagName('p')[1];
+let paragraphe = document.getElementsByTagName('p');
 
-console.log(paragraphe);
+console.log(paragraphe[1]);
 
-let liste = document.getElementsByTagName('li')[2];
+let liste = document.getElementsByTagName('li');
 
-console.log(liste);
+console.log(liste[2]);
