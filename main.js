0fa51920c9efb1f28286798db5531ce0130e2556
Jamila <jaja_mimi@hotmail.com>
2021-01-26 09:33:49 +0100

    exo fin

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1.
+let titre = document.getElementsByTagName('h1');
+
+console.log(titre[0]);
+//2.
+let soustitre1 = document.getElementsByTagName('h3')[0];
+let soustitre2 = document.getElementsByTagName('h3')[1];
+let soustitre3 = document.getElementsByTagName('h3')[2];
+
+console.log(soustitre1);
+console.log(soustitre2);
+console.log(soustitre3);
+
+//3.
+let paragraphe = document.getElementsByTagName('p')[1];
+
+console.log(paragraphe);
+
+let liste = document.getElementsByTagName('li')[2];
+
+console.log(liste);
